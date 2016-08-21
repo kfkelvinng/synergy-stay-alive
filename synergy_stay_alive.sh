1971ee8d03d2728b04dc6b7e1e63e56cd22a111d
Kelvin Ng <kfkelvinng@gmail.com>
2016-08-21 11:23:00 +0100

    Scan subnet

--- synergy_stay_alive.sh
+++ synergy_stay_alive.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-LOG="${DIR}/$(basename ${BASH_SOURCE[0]}).log"
+LOG="${DIR}/log.log"
 
 run() {
 
-  if /usr/sbin/arp -a | grep '(192.168.1.1)' | md5sum | egrep 'cbf645c49fb631ced617c459a50169f5|a11d00ddd31413fd44a8149bb2e58753'; then
+  if /usr/sbin/arp -a | grep '(192.168.1.1)' | md5sum | egrep 'cbf645c49fb631ced617c459a50169f5|a11d00ddd31413fd44a8149bb2e58753|45f677548faff833bf981b296786d824'; then
     log "At Home..."
   else
     log "Not at Home..."
@@ -13,7 +13,7 @@ run() {
   fi
 
   if netstat -apn 2>/dev/null | grep 24800 | tee -a "$LOG" | grep ESTABLISHED && pgrep synergyc ; then
-    log "Running..."
+    log "Already alive..."
     exit 0
   fi
 
@@ -26,9 +26,10 @@ run() {
   fi
 
   for dest in kelvin-pc 192.168.1.10{0..9}; do
-    log "Connecting to $dest"
+    log "Trying $dest"
     if nc -z $dest 24800; then
-      syn kelvin-pc
+      log "Connecting $dest"
+      syn $dest
       exit 0
     fi
   done
@@ -42,4 +43,8 @@ log() {
     echo $(date --rfc-3339=ns) $@ >> "$LOG"
 }
 
-run >> "$LOG" 2>&1
+(
+    flock -n 9 || ( log "flock"; exit 1 )
+    run >> "$LOG" 2>&1
+) 9>$DIR/lock
+
